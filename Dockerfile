baaa3bb9e85be363a37b029bb34251a0c4390267
Sami <sami.elleuch@insat.u-carthage.tn>
2020-08-11 17:54:46 +0100

    created yaml manifests for Sa and script for kubeconfig

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM debian:buster
+COPY ./sa.kubeconfig .
+RUN apt update && \
+      apt install -y curl && \
+      curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && \
+      chmod +x ./kubectl && \
+      mv ./kubectl /usr/local/bin/kubectl
+CMD kubectl --kubeconfig=sa.kubeconfig get po
---
+++ Kubectlconf-sa-gen.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# your server name goes here
+server=https://localhost:8443
+# the name of the secret containing the service account token goes here
+name=default-token-sg96k
+
+ca=$(kubectl get secret/$name -o jsonpath='{.data.ca\.crt}')
+token=$(kubectl get secret/$name -o jsonpath='{.data.token}' | base64 --decode)
+namespace=$(kubectl get secret/$name -o jsonpath='{.data.namespace}' | base64 --decode)
+
+echo "
+apiVersion: v1
+kind: Config
+clusters:
+- name: default-cluster
+  cluster:
+    certificate-authority-data: ${ca}
+    server: ${server}
+contexts:
+- name: default-context
+  context:
+    cluster: default-cluster
+    namespace: default
+    user: default-user
+current-context: default-context
+users:
+- name: default-user
+  user:
+    token: ${token}
+" > sa.kubeconfig
